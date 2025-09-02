cc55f7beb6062ff89db91fcdd42fa4ca3a89cc6f
pxyi0126 <pxyi@umich.edu>
2025-09-02 16:36:32 -0400

    this is not a funny name to me anymore, need to change repo name and deal with blank lines

--- parser_2016.py
+++ parser_2016.py
@@ -3,6 +3,41 @@
 # another for the variable listings
 # lastly one fo the questions
 # populate three lists?
+# %%
+import re
+import pandas as pd
 
-
-
+var_list = []
+sum_list = []
+cntr_list = []
+# %%
+with open("2016_vars.txt", "r") as f:
+    for line in f:
+        cntr_pattern= r'^(\d+)'
+        var_pattern = r'^V(\d+)'
+        if re.match(cntr_pattern, line) and '2016' not in line:
+            cntr_list.append(line.strip())
+        elif re.match(var_pattern, line):
+            var_list.append(line.strip())
+        else:
+            sum_list.append(line.strip())
+# %%
+with open("testing_cntr.txt", "w") as out_f:
+    out_f.writelines(cntr_list)
+with open("testing_var.txt", "w") as out_f:
+    out_f.writelines(var_list)
+with open("testing_sum.txt", "w") as out_f:
+    out_f.writelines(sum_list)
+# %%
+df2016 = pd.DataFrame(list(zip(cntr_list, var_list, sum_list)),
+                      columns = ['index', 'variable', 'question'])
+# %%
+df2016
+# %%
+csv2016 = df2016.to_csv("anes_2016_vars.csv", index=False)
+csv2016
+# %%
+# convert csv to xlsx
+read_file = pd.read_csv("anes_2016_vars.csv")
+read_file.to_excel("anes_2016_vars.xlsx", index=None, header=True)
+# %%
