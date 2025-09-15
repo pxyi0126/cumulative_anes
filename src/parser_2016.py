3861164db6fc993671aec86fc25734f267de3df3
pxyi0126 <pxyi@umich.edu>
2025-09-15 13:42:48 -0400

    reorganized repo

---
+++ src/parser_2016.py
@@ -0,0 +1,52 @@
+# %%
+import re
+import pandas as pd
+
+var_list = []
+sum_list = []
+cntr_list = []
+# %%
+with open("2016_vars.txt", "r") as f:
+    for line in f:
+        cntr_pattern= r'^(\d+)'
+        var_pattern = r'^V(\d+)'
+        if re.match(cntr_pattern, line) and '2016' not in line:
+            cntr_list.append(line.strip())
+        elif re.match(var_pattern, line):
+            var_list.append(line.strip())
+        else:
+            sum_list.append(line.strip())
+
+# %%
+print(cntr_list)
+print(len(cntr_list)) #1839
+filter_cntr = [x for x in cntr_list if x != '']
+print(len(filter_cntr)) #1841
+
+print(var_list)
+print(len(var_list)) #1841
+filter_var = [x for x in var_list if x != '']
+print(len(filter_var)) #1841
+
+print(sum_list)
+print(len(sum_list)) #1964
+filter_sum = [x for x in sum_list if x != '']
+print(len(filter_sum)) #1842...??
+# %%
+df2016 = pd.DataFrame(list(zip(filter_cntr, filter_var, filter_sum)),
+                      columns = ['index', 'variable', 'question'])
+# %%
+
+csv2016 = df2016.to_csv("parsed_2016.csv", index=False)
+csv2016
+# %%
+# convert csv to xlsx
+read_file = pd.read_csv("parsed_2016.csv")
+read_file.to_excel("parsed_2016.xlsx", index=None, header=True)
+# %%
+# I now need to check if the parsed and original lists match
+import pandas as pd
+data2 = pd.read_excel("2016_orig.xlsx")
+data1 = pd.read_excel("anes_2016_vars.xlsx")
+
+data2
---
+++ test/check.py
@@ -0,0 +1,16 @@
+import difflib
+import pandas as pd
+
+df2016 = pd.read_csv("parsed_2016.csv")
+df20162 = pd.read_excel("2016_var.xlsx")
+df20162.to_csv("2016_var.csv", index=False)
+# df20162['1'] = df20162['1'].astype(str)
+
+col1 = df2016["variable"].astype(str).tolist()
+col2 = df20162['version'].astype(str).tolist()
+
+diff = difflib.unified_diff(col1, col2, fromfile='parsed_2016.csv', tofile="2016_var.csv")
+for line in diff:
+    print(line)
+
+print(len(col1), len(col2))
