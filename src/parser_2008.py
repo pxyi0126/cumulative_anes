6283ea4b5d7f808ab32286336b1e22ba17f04534
pxyi0126 <pxyi@umich.edu>
2025-09-18 15:44:30 -0400

    pdf data, wrote some more to parse 2008

--- src/parser_2008.py
+++ src/parser_2008.py
@@ -1,2 +1,15 @@
 import pandas as pd
+import re
+
+
+var_code_pattern = re.compile(r"V08\d+")
+description_pattern = re.compile(r"\s{4}\w+.\d.\s(\w.*$)")
+section_separator = "============================================================================="
+
+with open("small2008.txt", "r", encoding="utf-8") as f:
+    lines = f.read()
+
+
+
+
 
