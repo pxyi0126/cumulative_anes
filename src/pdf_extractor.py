6ea68aabc3be763b1f7c5ef39da875c68d1e0c78
pxyi0126 <pxyi@umich.edu>
2025-09-24 17:42:53 -0400

    24 csv work

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -1,7 +1,8 @@
+# %%
 from pypdf import PdfReader
 import glob
 import os
-
+# %%
 def pdf_extract(file_path):
     # Create PdfReader object
     reader = PdfReader(file_path)
@@ -25,8 +26,75 @@ files = glob.glob("data/pdf_data/*.pdf", recursive=True)
 for file in files:
     pdf_extract(file)
 
+# %%
+# Parsing through 2024 codebook txt
+import re
+import pandas as pd
+var_pattern = re.compile(r"(V24\d+[a-d,z]*)(.*)$")
+rows = []
 
-    #need three things in three columns
-    #1. Variable
-    #2. valid values
-    #3. missing values
+def parse_24(lines, start_idx):
+    line = lines[start_idx].strip()
+    match = var_pattern.match(line)
+    if not match:
+        return start_idx + 1
+    var_name = match.group(1).strip()
+    summary = match.group(2).strip()
+
+    question, survey_q, universe, note = "", "", "", ""
+    missing_label, valid_label = "", ""
+    i = start_idx + 1
+    while i < len(lines):
+        line = lines[i].strip()
+        if var_pattern.match(line) or line.startswith("WEIGHTING VARIABLES"):
+            break
+        if line.startswith("Value Labels"):
+            i += 1
+            while line < len(lines) and re.match(r"^-?\d+\..*", line.strip()):
+                label_line = lines[i].strip()
+                if re.match(r"^\-\d+\..*$", label_line):
+                    missing_label += label_line + "\n"
+                else:
+                    valid_label += label_line + "\n"
+                i+=1
+            continue
+
+        if line.startswith("Question"):
+            question += " " + line.replace("Question", "").strip()
+        elif line.startswith("Survey Question"):
+            survey_qs += " " + line.replace("Survey Question(s)", "").strip()
+        elif line.startswith("Universe"):
+            universe += " " + line.replace("Universe", "").strip()
+        elif line.startswith("Note"):
+            note += " " + line.replace("Note", "").strip()
+        else:
+            summary += " " + line.strip()
+
+        i += 1
+
+    rows.append((
+        var_name,
+        summary.strip(),
+        question.strip(),
+        survey_qs.strip(),
+        universe.strip(),
+        note.strip(),
+        missing_label.strip(),
+        valid_label.strip()
+    ))
+    return i
+
+
+
+with open("data/pdf_data/2024_codebook.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+# weighting variables
+weighting_vars = []
+
+i = 0
+while i < len(lines):
+    line = lines[i].strip()
+    if line.startswith("WEIGHTING VARIABLES"):
+        i += 1
+
+# %%
