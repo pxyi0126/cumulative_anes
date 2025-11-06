0a62a13dda8472874745b99ccf1cf70316c626e4
pxyi0126 <pxyi@umich.edu>
2025-11-06 17:54:44 -0500

    2008 parser works on the test now

--- src/parser/parser_2008.py
+++ src/parser/parser_2008.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 import os
 # import shutil
-os.chdir("/Users/yipho/anes/cumulative_anes/data/raw/txt")
+os.chdir("/Users/yipho/anes/cumulative_anes/test")
 
 var_pattern = re.compile(r"(V\d+[a-z,_orig]*)(.*)$")
 val_pattern = re.compile(r"^(\d+)\.\s*([A-Za-z].*?)(?=\s+\d|\s*$)")
@@ -39,11 +39,10 @@ def parse_08(lines, start_idx):
 
     while i < len(lines):
         line = lines[i].strip()
-
-        if re.match(r"^V\d+", line):
+        if line.startswith("TYPE:") or line.startswith("NOTES:"):
             break
 
-        if divider.match(line):
+        if divider.match(line) and not question:
             block_lines = []
             i += 1
 
@@ -56,23 +55,37 @@ def parse_08(lines, start_idx):
             if i < len(lines) and divider.match(lines[i].strip()):
                 i += 1
             continue
+
         if "VALID CODES" in line.upper():
             print(f"Found VALID CODES for {varname}")
             i += 2
-            while i < len(lines) and :
-                val_line = lines[i].strip()
-            val_line = val_pattern.match(lines[i].strip())
-
-
+            while i < len(lines):
+                line = lines[i].strip()
+                if not line or line.startswith("MISSING CODES:") or re.match(r"^V\d+", line):
+                    break
+                val_match = val_pattern.match(line)
+                if val_match:
+                    valid_label += val_match.group() + "\n"
+                i += 1
+            continue
 
         if line.startswith("MISSING CODES:"):
             i += 2
-            print(line)
-
+            print(f"Found MISSING CODES for {varname}")
+            while i < len(lines):
+                line = lines[i].strip()
+                if not line or re.match(r"^V\d+", line):
+                    break
+                miss_match = missing_pattern.match(line)
+                if miss_match:
+                    missing_label += miss_match.group() + "\n"
+                i += 1
+            continue
         i += 1
     rows.append((varname, summary, question, valid_label.strip(), missing_label.strip()))
     return i, rows
-with open("2008_pre.txt", "r", errors='replace') as f:
+
+with open("2008test.txt", "r", errors='replace') as f:
     lines = f.readlines()
 i = 0
 while i < len(lines):
@@ -81,6 +94,7 @@ while i < len(lines):
         i, rows = parse_08(lines, i)
     else:
         i += 1
+
 df_2008 = pd.DataFrame(rows, columns=['var_name', 'summary', 'question', 'valid_labels', 'missing_labels'])
 df_2008.to_csv("2008_pcodebook.csv", index=False, encoding="utf-8")
 
